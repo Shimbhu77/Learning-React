4725ceedde0a0c86b6965794653bfb1386326d21
Shimbhu77 <kumawatshimbhu4@gmail.com>
2023-06-05 19:02:58 +0530

    youtube api uses

---
+++ LearningLoop/learning_loop/src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Album from './Album'
+
+export default function Navbar() {
+  return (
+    <>
+       <Album/>
+    </>
+  )
+}
---
+++ LearningLoop/learning_loop/src/Components/Video.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Video() {
+  return (
+    <div>
+        <iframe width="420" height="315"
+              src="https://www.youtube.com/embed/tgbNymZ7vqY">
+        </iframe>
+    </div>
+  )
+}
