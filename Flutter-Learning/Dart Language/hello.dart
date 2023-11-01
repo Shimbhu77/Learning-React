e1986e6994a526b476d40c4fda41e98c5920a9cc
Shimbhu77 <kumawatshimbhu4@gmail.com>
2023-11-01 21:50:27 +0530

    Flutter learning day 1 code

---
+++ Flutter-Learning/Dart Language/hello.dart
@@ -0,0 +1,22 @@
+void main()
+{
+
+     // Day 1
+          // Hello Dart
+          print("Hello Shimbhu!");
+
+          // Variable values
+
+          int value = 77;
+          String name = "shimbhu";
+          bool isDeveloper = true;
+          dynamic isHappy="something went wrong.";
+
+          print(value);
+          print(name);
+          print(isDeveloper);
+          print(isDeveloper);
+
+      // Day 2
+          
+   }
