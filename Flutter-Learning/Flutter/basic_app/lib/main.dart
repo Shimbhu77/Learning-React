b0aa75c0e2a974c59f4334b0fedb70be06c9878d
Shimbhu77 <shimbhu.musterdekho@gmail.com>
2023-12-06 08:16:27 +0530

    theme color and font size universal.

--- Flutter-Learning/Flutter/basic_app/lib/main.dart
+++ Flutter-Learning/Flutter/basic_app/lib/main.dart
@@ -13,24 +13,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+          displayMedium: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.cyan),
+          titleSmall: TextStyle(fontSize: 50,fontWeight: FontWeight.bold,color: Colors.purple)
+        )
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Shimbhu'),
     );
@@ -84,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Try changing the color here to a specific color (to
           // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
           // change color while the other colors stay the same.
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
@@ -115,11 +106,11 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               Text(
                 "Shimbhu Kumawat",
-                style: TextStyle(fontSize: 30, fontFamily: 'RubikFamily',color: Colors.blue),
+                style: Theme.of(context).textTheme.displayMedium,
               ),
               Text(
                 "Payal Kumawat",
-                style: TextStyle(fontSize: 30, fontFamily: 'RubikFamily',color: Colors.pink),
+                style: Theme.of(context).textTheme.titleSmall,
               )
             ],
           ),
